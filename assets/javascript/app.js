f7f543ce728ea6693a463be2ec5245ab3cec1beb
Odysseus <arwaterman@gmail.com>
2019-03-03 14:12:02 -0800

    added time out tracker in order to clear timer when the user gives an answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,6 +12,8 @@ let game = {
                 "duck"],
     choices:   [["9 meters per second", "11 meters per second", "13 meters per second", "15 meters per second"],
                 ["gull", "falcon", "goose", "duck"]],
+    
+    timeOutTracker: [],
 
     nextQuestion: function() { this.qIndex++; },
     answeredRight: function() { this.aRight++; },
@@ -80,20 +82,23 @@ function startTimer(seconds) {
     //   so lets store the length value first
     let length = timeAry.length;
     
+    // game.timeOutTracker = [];
     for (i=0; i<length; i++) {
         let t = timeAry.pop();
         // console.log(t);
 
         if (t > 0) {
-            setTimeout(function() {
+            game.timeOutTracker.push(
+                setTimeout(function() {
                 let timeSection = document.getElementById("time");
     
                 timeSection.innerHTML = "<h2>Time Remaining: " + t + " left</h2>";
     
-            }, ((t - seconds) * -1) * 1000);
+            }, ((t - seconds) * -1) * 1000));
         }
         else {
-            setTimeout(function() {
+            game.timeOutTracker.push(
+                setTimeout(function() {
                 let timeSection = document.getElementById("time");
                 let questionSection = document.getElementById("question");
                 let answersSection = document.getElementById("answers");
@@ -103,19 +108,18 @@ function startTimer(seconds) {
                 answersSection.innerHTML = "<p>The answer was: " + game.answers[game.qIndex] + "</p>";
                 game.answeredWrong();
     
-            }, ((t - seconds) * -1) * 1000);
+            }, ((t - seconds) * -1) * 1000));
         }   
     }
 }
 
 function clickOnAnswer() {
-    // console.log(this);
+
+    for (i in game.timeOutTracker) {
+        clearTimeout(game.timeOutTracker[i]);
+    }
 
     let answer = this.getAttribute("data-value");
-    // console.log(answer);
-
-    // console.log(game.choices[game.qIndex][answer]);
-    // console.log(game.answers[game.qIndex]);
 
     if (game.choices[game.qIndex][answer] === game.answers[game.qIndex]) {
         game.answeredRight();
